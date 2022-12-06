f3bb3a4d34cca4704dab19db8c2c7b0e89ba0273
TomReilly1 <reilly.thomas.c@gmail.com>
2022-12-06 15:40:14 -0500

    day-1: part two

--- day-1/main.py
+++ day-1/main.py
@@ -1,20 +1,23 @@
 def main() -> None:
     counter = 0
-    max = 0
+    max_values = [0, 0, 0]
 
     with open('input.txt', 'r') as f:
         lines = f.readlines()
 
-        
         for line in lines:
             if line != '\n':
                 counter += int(line)
             else:
-                if counter > max:
-                    max = counter
+                min_value = min(max_values)
+                min_index = max_values.index(min_value)
+
+                if counter > min_value:
+                    max_values[min_index] = counter
+
                 counter = 0
             
-    print(max)
+    print(sum(max_values))
 
 
 if __name__ == '__main__':
